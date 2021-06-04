2afc79bc4c1e4cb9b64e9306bd85acc370896f97
AakarshN <77533089+AakarshN@users.noreply.github.com>
2021-06-04 18:05:12 +0530

    added

---
+++ Sling.js
@@ -0,0 +1,30 @@
+class Sling{
+    constructor(bodyA, pointB){
+       var options= {
+        'bodyA': bodyA,
+        'pointB': pointB,
+        'stiffness': 0.04,
+        'length': 10
+       }
+       this.pointB= pointB
+       this.sling = Constraint.create(options);
+
+       World.add(world,this.sling);
+    }
+
+    display(){
+        if(this.sling.bodyA !== null){
+            var A = this.sling.bodyA.position;
+            var B= this.pointB
+            push();
+            stroke("#24FAE0")
+            line(A.x,A.y,B.x,B.y)
+            pop();
+        }
+    }
+
+    fly(){
+        this.sling.boadyA= null
+    }
+
+}
